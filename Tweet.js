5dbc785a6331f35c894741eda678ae4ab50ef246
ojvwe <ojvwebdesign@gmail.com>
2023-05-25 08:34:28 -0400

    second update

---
+++ Tweet.js
@@ -0,0 +1,11 @@
+// Tweet: a single tweet using props.
+function Tweet({ date, message, name, username }) {
+    return (
+        <div className="tweet">
+            <span>{name}</span>
+            <span className="username">{username}</span>
+            <span className="date">{date}</span>
+            <p>{message}</p>
+        </div>
+    );
+}
